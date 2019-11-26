9cba0f2e5f2f33137e94e9f538609d04a97695bc
fedPass <federica.passanante@gmail.com>
2019-11-26 18:12:07 +0100

    aggiustate voci dropdown menu

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.menu-item').click(function() {
         $('.dropdown-content').slideUp();
-        if (!$(this).children('.dropdown-content').is(':visible')) {
+        if ($(this).children('.dropdown-content').is(':hidden')) {
             $(this).children('.dropdown-content').slideDown();
         }
     });
