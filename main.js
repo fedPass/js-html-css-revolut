ad5c6b6919bb8e4c0bdb813c8e1ecc6c451098b8
fedPass <federica.passanante@gmail.com>
2019-11-26 18:53:42 +0100

    provato a manipolare con js icona di scelta lingua

--- main.js
+++ main.js
@@ -4,5 +4,9 @@ $(document).ready(function(){
         if ($(this).children('.dropdown-content').is(':hidden')) {
             $(this).children('.dropdown-content').slideDown();
         }
+        if ($('.dropdown-content.languages').is(':visible')) {
+            $('.fas.fa-caret-down').removeClass('fa-caret-down').addClass('fa-caret-up');
+        } else {
+            $('.fas.fa-caret-up').removeClass('fa-caret-up').addClass('fa-caret-down');}
     });
 });
