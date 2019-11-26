2ed22ccaf2447a41a05a10c71dd3819225aceefa
fedPass <federica.passanante@gmail.com>
2019-11-26 17:50:19 +0100

    inserito js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    
+    $('.menu-item').click(function() {
+        $('.dropdown-content').slideUp();
+        if (!$(this).children('.dropdown-content').is(':visible')) {
+            $(this).children('.dropdown-content').slideDown();
+        }
+    });
 });
